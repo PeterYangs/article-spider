4dbe0113f13487768ca067306a37f6767759c77b
Peter Yang <904801074@qq.com>
2021-03-14 20:37:54 +0800

    优化

--- test/list.go
+++ test/list.go
@@ -18,11 +18,14 @@ func main() {
 		ListHrefSelector: "div.col-md-8.col-xs-8 > a",
 		DetailFields: map[string]form.Field{
 			"title": {Types: fileTypes.SingleField, Selector: "body > section > div > div > div.col-md-7 > div:nth-child(2) > div > h3"},
-			"image": {Types: fileTypes.SingleImage, Selector: "body > section > div > div > div.col-md-3 > div > div.info-panel > ul > li:nth-child(1) > img.left"},
+			//"image": {Types: fileTypes.SingleImage, Selector: "body > section > div > div > div.col-md-3 > div > div.info-panel > ul > li:nth-child(1) > img.left"},
 			//body > section > div > div > div.col-md-3 > div > div.info-panel > ul > li:nth-child(1) > img.left
 			//"html":    {Types: fileTypes.OnlyHtml, Selector: "body > section > div > div > div.col-md-7 > div:nth-child(2) > div"},
 			//"image":   {Types: fileTypes.SingleImage, Selector: "body > section > div > div > div.col-md-3 > div > div.qrcode-panel.common-panel > div:nth-child(1) > img", ImagePrefix: "upload", ImageDir: "[date:Ym]/[random:1-100]"},
 			//"content": {Types: fileTypes.HtmlWithImage, Selector: "#detail-content", ImagePrefix: "upload", ImageDir: "[date:Ym]/[random:1-100]"},
+		},
+		ListFields: map[string]form.Field{
+			"image": {Types: fileTypes.SingleImage, Selector: "div > img"},
 		},
 	}
 
