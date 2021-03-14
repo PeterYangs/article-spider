0e8bab871677d3b65506fdf707f015d83f51c9cc
Peter Yang <904801074@qq.com>
2021-03-14 15:40:57 +0800

    打包文件夹

---
+++ dist/win/web/api.go
@@ -0,0 +1,105 @@
+package web
+
+import (
+	"article-spider/fileTypes"
+	"article-spider/form"
+	"article-spider/spider"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+//启动web服务
+func StartWeb() {
+
+	r := gin.Default()
+
+	r.Delims("{[{", "}]}")
+
+	r.Static("/static", "./web/static")
+
+	r.LoadHTMLGlob("./web/html/*")
+
+	r.GET("/ping", func(context *gin.Context) {
+
+		context.JSON(200, gin.H{"msg": "success"})
+
+	})
+
+	r.GET("/", func(context *gin.Context) {
+
+		context.HTML(200, "index.html", gin.H{})
+	})
+
+	r.POST("/submit", func(context *gin.Context) {
+
+		//post:=context.Request.ParseForm
+		//
+		json := make(map[string]interface{}) //注意该结构接受的内容
+		err := context.BindJSON(&json)
+
+		if err != nil {
+
+			fmt.Println(err)
+
+			return
+		}
+
+		//fmt.Println(json)
+		//
+		//context.JSON(200, gin.H{"data": post})
+
+		limit, err := strconv.Atoi(json["limit"].(string))
+
+		if err != nil {
+
+			fmt.Println(err)
+
+			return
+		}
+
+		pageStart, err := strconv.Atoi(json["pageStart"].(string))
+
+		if err != nil {
+
+			fmt.Println(err)
+
+			return
+		}
+
+		detailFields := make(map[string]form.Field)
+
+		for i, v := range (json["detailFields"]).(map[string]interface{}) {
+
+			item := v.(map[string]interface{})
+
+			types := item["types"]
+
+			detailFields[i] = form.Field{Types: fileTypes.FieldTypes((types).(float64)), Selector: (item["selector"]).(string)}
+		}
+
+		f := form.Form{
+			Host:             (json["host"]).(string),
+			Channel:          (json["channel"]).(string),
+			Limit:            limit,
+			PageStart:        pageStart,
+			ListSelector:     (json["listSelector"]).(string),
+			ListHrefSelector: (json["listHrefSelector"]).(string),
+			DetailFields:     detailFields,
+		}
+
+		//fmt.Println(f)
+
+		//for i,v:=range f {
+		//
+		//
+		//
+		//}
+
+		go spider.Start(f)
+
+	})
+
+	r.Run(":8089")
+
+}
