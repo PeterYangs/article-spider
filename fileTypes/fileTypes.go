058b6e2be245606781a05334378248e0b398b248
PeterYangs <904801074@qq.com>
2021-10-11 10:57:40 +0800

    注释添加

--- fileTypes/fileTypes.go
+++ fileTypes/fileTypes.go
@@ -9,4 +9,4 @@ const HtmlWithImage FieldTypes = 0x00004  //html包括图片
 const MultipleImages FieldTypes = 0x00005 //多图
 const Attr FieldTypes = 0x00006           //标签属性选择器
 const Fixed FieldTypes = 0x00007          //固定数据，填什么返回什么,选择器就是返回的数据
-const Regular FieldTypes = 0x00008        //正则
+const Regular FieldTypes = 0x00008        //正则（FindStringSubmatch,返回一个结果）
