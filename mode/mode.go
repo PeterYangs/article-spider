e94576f23e5e8763fff09a1d7323d1c7b6c8cb85
PeterYangs <904801074@qq.com>
2021-10-21 16:18:35 +0800

    变量定义优化

--- mode/mode.go
+++ mode/mode.go
@@ -2,6 +2,8 @@ package mode
 
 type Mode int
 
-const Normal Mode = 0x00000 //常规模式
-const Api Mode = 0x00001    //api模式
-const Auto Mode = 0x00002   //自动化模式
+const (
+	Normal Mode = 0x00000 //常规模式
+	Api    Mode = 0x00001 //api模式
+	Auto   Mode = 0x00002 //自动化模式
+)
