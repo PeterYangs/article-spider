e01b8e8139fec6febcd19ba430a17b461cec5d87
PeterYangs <904801074@qq.com>
2022-08-13 11:38:56 +0800

    mode的ToString方法

--- mode.go
+++ mode.go
@@ -9,6 +9,31 @@ const (
 	Url    Mode = 0x00003 //详情页链接模式
 )
 
+func (m Mode) ToString() string {
+
+	switch m {
+
+	case 0x00000:
+
+		return "Normal"
+
+	case 0x00001:
+
+		return "Api"
+
+	case 0x00002:
+
+		return "Auto"
+
+	case 0x00003:
+
+		return "Url"
+
+	}
+
+	return ""
+}
+
 type NextPageMode int
 
 const (
