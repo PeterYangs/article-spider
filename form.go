d07eea40309c3d8fd6ee76eed951912c7bbe8885
PeterYangs <904801074@qq.com>
2022-10-09 13:39:22 +0800

    添加前置排除选择器

--- form.go
+++ form.go
@@ -53,7 +53,8 @@ type Form struct {
 type Field struct {
 	Types              FieldTypes
 	Selector           string                                              //字段选择器
-	NotSelector        []string                                            //剔除选择器
+	NotSelector        []string                                            //剔除选择器(后置选择器，意思是先获取该item的doc再剔除节点)
+	PrefixNotSelector  []string                                            //前置剔除选择器(意思是先剔除html的节点)
 	AttrKey            string                                              //属性值参数
 	ImagePrefix        func(form *Form, imageName string) string           //图片路径前缀,会添加到图片路径前缀，但不会生成文件夹
 	ImageDir           string                                              //图片子文件夹，支持变量 1.[date:Y-m-d] 2.[random:1-100] 3.[singleField:title]
@@ -261,7 +262,7 @@ func (f *Form) ResolveSelector(html string, selector map[string]Field, originUrl
 	var globalErr error = nil
 
 	//goquery加载html
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	htmlDoc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
 
 	if err != nil {
 
@@ -271,6 +272,14 @@ func (f *Form) ResolveSelector(html string, selector map[string]Field, originUrl
 
 	//解析详情页面选择器
 	for fieldT, itemT := range selector {
+
+		doc := htmlDoc
+
+		//前置剔除选择器
+		for _, s := range itemT.PrefixNotSelector {
+
+			doc.Find(s).Remove()
+		}
 
 		field := fieldT
 
