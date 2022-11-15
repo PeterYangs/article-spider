9df875bbb8aaf7abadd57298d23453c8ddf1e9f3
PeterYangs <904801074@qq.com>
2022-11-15 12:33:40 +0800

    调试

--- normal.go
+++ normal.go
@@ -92,7 +92,7 @@ func (n normal) GetList(listUrl string) {
 		var rows *Rows = NewRows(map[string]string{})
 
 		//a链接是列表的情况
-		if strings.TrimSpace(n.s.form.HrefSelector) == "" {
+		if strings.TrimSpace(n.s.form.HrefSelector) == "" || n.s.form.HrefSelector == "myself" {
 
 			href, isFind = s.Attr("href")
 
